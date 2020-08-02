e5d6b3a120411800cad10782826fed665b3c6115
lay0020 <lay0020@gmail.com>
2020-08-02 11:47:50 +0800

    抓取json資料

--- ajax.js
+++ ajax.js
@@ -4,3 +4,34 @@ var xhr = new XMLHttpRequest();  // 跟伺服器要求資料的物件
 
 xhr.open("GET", "./data.txt");   // 開啟檔案("取得" , "檔案路徑" , 非同步處理)
 xhr.send();                      // 傳送
+
+//載入完成後會執行
+// 不用更新頁面,會自動同步
+
+xhr.onload = () => {
+    content.innerText = xhr.responseText;
+}
+
+
+
+
+
+//JSON
+// json 檔案資料: 不可以加註解
+//  "名稱-以英文為主避免亂碼" : "值-資料-中英文皆可"
+
+var product = document.getElementById("product");
+var price = document.getElementById("price");
+
+var xhrJson = new XMLHttpRequest();
+
+xhrJson.open("GET", "./data.json");
+xhrJson.send();
+
+xhrJson.onload = ()=> {
+    //JSON.格式(想轉為 JSON 內容) 
+    var json = JSON.parse(xhrJson.responseText);
+
+    product.innerText = json["product"];
+    price.innerText = json["price"];
+}
