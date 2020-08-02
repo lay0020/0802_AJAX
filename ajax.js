2157066973cfe42fc31cee9cd50e85877f43f3a8
lay0020 <lay0020@gmail.com>
2020-08-02 11:06:03 +0800

    取得AJAX 資料

--- ajax.js
+++ ajax.js
@@ -1,1 +1,6 @@
 var content = document.getElementById("content");
+
+var xhr = new XMLHttpRequest();  // 跟伺服器要求資料的物件
+
+xhr.open("GET", "./data.txt");   // 開啟檔案("取得" , "檔案路徑" , 非同步處理)
+xhr.send();                      // 傳送
